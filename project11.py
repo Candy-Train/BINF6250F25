e1df5f4da2f5fbe53085f899dfc06ebfad8454db
Chris Fitzgerald <63211445+Candy-Train@users.noreply.github.com>
2025-12-10 10:53:38 -0500

    Add files via upload

--- project11.py
+++ project11.py
@@ -8,8 +8,19 @@ import pandas as pd           # Simple convergence checks
 from itertools import product # For robust iteration
 from array import array       # Faster base Python iteration
 
-def build_profile_from_fasta(fasta_file):
 
+
+def build_profile_from_fasta(fasta_file):
+    '''
+    Purpose - to open a FASTA file and create a profile array that indicates if a position is a Match or Insertion State.
+
+    Inputs - 
+        A FASTA file where all sequences are the same length
+
+    Output -
+        column state array, the length of the sequences in the FASTA file where each position is the state of the column (Numpy array)
+        total sequence array, where each row is a sequence and each column position is a position in the sequence. (Numpy array)
+    '''
     sequences = []
 
     with open(fasta_file) as file:
@@ -58,8 +69,88 @@ def build_profile_from_fasta(fasta_file):
 
 
 
+def generate_labeled_residue_array(total_sequence_array, column_state_array):
+    '''
+    Purpose - Creates a matrix that is the same size as the total_sequence_array, where each position is the state
+            of the residue in the same position in the total_sequence_array
+
+    Input - 
+        column_state_array, the length of the sequences in the FASTA file where each position is the state of the column (Numpy array)
+        total_sequence_array, where each row is a sequence and each column position is a position in the sequence. (Numpy array)
+
+    Output - 
+        labeled_residue_array, a matrix the same size as the total_sequence_array, which holds the state 
+                            assigned to the residue in the same location in the total_sequence_array
+    '''
+    labeled_residue_array = np.empty_like(total_sequence_array, dtype="U2")
+    for row in range(len(total_sequence_array[:,0])):
+        counter = 1
+        for column, state in enumerate(column_state_array):
+            if state[0] == "M" and total_sequence_array[row, column] != "-":
+                labeled_residue_array[row, column] = "M" + str(counter)
+                counter += 1
+            elif column_state_array[column][0] == "M" and total_sequence_array[row, column] == "-":
+                labeled_residue_array[row, column] = "D" + str(counter)
+                counter += 1
+            elif column_state_array[column][0] == "I" and total_sequence_array[row, column] != "-":
+                labeled_residue_array[row, column] = "I" + str(counter - 1)
+            elif column_state_array[column][0] == "I" and total_sequence_array[row, column] == "-":
+                labeled_residue_array[row, column] = "--"
+    return labeled_residue_array
+
+
+def generate_states_and_transitions(labeled_residue_array):
+    '''
+    Purpose - Generates the list hidden states and initializes the trans_probs and intitial_probs dicts to store all possible transitions for the pHMM
+    
+    Input - 
+        labeled_residue_array, a matrix the same size as the total_sequence_array, which holds the state 
+                            assigned to the residue in the same location in the total_sequence_array
+    
+    Output -
+        hiddenstate - a list of all possible states in the pHMM
+        trans_probs - a dict of dict of floats to hold the probabilities of all possible state transitions in the model
+        initial_probs - a dict of floats to hold the initital state probabilites of the model
+    '''
+    hiddenstates = []
+    pHMM_length = int(labeled_residue_array[-1, -1][1])
+    for position in range(pHMM_length + 1):
+        hiddenstates.append("M" + str(position))
+    for position in range(pHMM_length + 1):
+        hiddenstates.append("I" + str(position))
+    for position in range(1, pHMM_length + 1):
+        hiddenstates.append("D" + str(position))
+    hiddenstates.append("M" + str(pHMM_length + 1))
+
+    trans_probs = defaultdict()
+    for state in hiddenstates[:-1]:
+        position = int(state[1])
+        possible_trans = {"M" + str(position + 1): 0, "I" + str(position): 0, "D" + str(position + 1): 0}
+        if ("D" + str(pHMM_length + 1)) in possible_trans:
+            del possible_trans["D" + str(pHMM_length + 1)]
+        trans_probs[state] = possible_trans
+
+
+    initial_probs = defaultdict()
+    for state in hiddenstates:
+        initial_probs[state] = 0
+    initial_probs[hiddenstates[0]] = 1
+
+    return hiddenstates, trans_probs, initial_probs
+
+
+
 def compute_emission_probabilities(column_state_array, total_sequence_array):
+        '''
+        Purpose - calculates the emission probabilities based on our initial sequences and states
+
+        Input -
+            column state array, the length of the sequences in the FASTA file where each position is the state of the column (Numpy array)
+            total sequence array, where each row is a sequence and each column position is a position in the sequence. (Numpy array)
         
+        Output -
+            emission_probs, a dict of dict of floats that contains the emission probabilities for states that have emissions
+        '''
         emission_probs = {}
         insertion_residue_counts = {}
 
@@ -106,60 +197,21 @@ def compute_emission_probabilities(column_state_array, total_sequence_array):
 
         return emission_probs
 
-    
-
-def generate_labeled_residue_array(total_sequence_array, column_state_array):
-    labeled_residue_array = np.empty_like(total_sequence_array, dtype="U2")
-    for row in range(len(total_sequence_array[:,0])):
-        counter = 1
-        for column, state in enumerate(column_state_array):
-            if state[0] == "M" and total_sequence_array[row, column] != "-":
-                labeled_residue_array[row, column] = "M" + str(counter)
-                counter += 1
-            elif column_state_array[column][0] == "M" and total_sequence_array[row, column] == "-":
-                labeled_residue_array[row, column] = "D" + str(counter)
-                counter += 1
-            elif column_state_array[column][0] == "I" and total_sequence_array[row, column] != "-":
-                labeled_residue_array[row, column] = "I" + str(counter - 1)
-            elif column_state_array[column][0] == "I" and total_sequence_array[row, column] == "-":
-                labeled_residue_array[row, column] = "--"
-    return labeled_residue_array
 
 
-def generate_states_and_transitions(labeled_residue_array):
+def compute_transition_probabilities(trans_probs, labeled_residue_array):
     '''
-    Docstring for function_3
+    Purpose - calculates the transition probabilities from all observed transitions
+
+    Input - 
+        labeled_residue_array, a matrix the same size as the total_sequence_array, which holds the state 
+                            assigned to the residue in the same location in the total_sequence_array
+        trans_probs, a dict of dict of floats to hold the probabilities of all possible state transitions in the model
     
-    :param labeled_residue_array: Description
+    Output -
+        trans_probs, a dict of dict of floats that now holds the probabilities of all possible state transitions in the model
+        observed_transitions, a dict of dict of ints that holds the counts for each observed transition from one (outer) state to another (inner) state
     '''
-    hiddenstates = []
-    pHMM_length = int(labeled_residue_array[-1, -1][1])
-    for position in range(pHMM_length + 1):
-        hiddenstates.append("M" + str(position))
-    for position in range(pHMM_length + 1):
-        hiddenstates.append("I" + str(position))
-    for position in range(1, pHMM_length + 1):
-        hiddenstates.append("D" + str(position))
-    hiddenstates.append("M" + str(pHMM_length + 1))
-
-    trans_probs = defaultdict()
-    for state in hiddenstates[:-1]:
-        position = int(state[1])
-        possible_trans = {"M" + str(position + 1): 0, "I" + str(position): 0, "D" + str(position + 1): 0}
-        if ("D" + str(pHMM_length + 1)) in possible_trans:
-            del possible_trans["D" + str(pHMM_length + 1)]
-        trans_probs[state] = possible_trans
-
-
-    initial_probs = defaultdict()
-    for state in hiddenstates:
-        initial_probs[state] = 0
-
-    return hiddenstates, trans_probs, initial_probs
-
-
-
-def estimate_parameters(trans_probs, labeled_residue_array):
     observed_transitions = copy.deepcopy(trans_probs)
 
     #gets M0 start state transitions
@@ -195,44 +247,46 @@ def estimate_parameters(trans_probs, labeled_residue_array):
 
     return observed_transitions, trans_probs
 
+
+
 #FORWARD
 def forward_sequence(sequence, model):
-    model.hidden_states.pop(0)
     forward_prob = model.forward(sequence)
     log_prob = np.log10(forward_prob)
     return forward_prob, log_prob
+
 
 #VITERBI
 def viterbi_sequence(sequence, model):
     return model.viterbi(sequence)
 
-column_state_array, total_sequence_array = build_profile_from_fasta("phmm_train_motif1.fasta")
-labeled_residue_array = generate_labeled_residue_array(total_sequence_array, column_state_array)
-hiddenstates, trans_probs, initial_probs = generate_states_and_transitions(labeled_residue_array)
-observed_transitions, trans_probs = estimate_parameters(trans_probs, labeled_residue_array)
-emission_probs = compute_emission_probabilities(column_state_array, total_sequence_array)
 
+def main():
 
-for initial_state in trans_probs["M0"]:
-    initial_probs[initial_state] = trans_probs["M0"][initial_state]
+    fasta_file = "phmm_train_motif1.fasta"
 
-test_sequence = "VGQDH"
+    alphabet = "ARNDCEQGHILKMFPSTWYV"
+    column_state_array, total_sequence_array = build_profile_from_fasta(fasta_file)
+    labeled_residue_array = generate_labeled_residue_array(total_sequence_array, column_state_array)
+    hiddenstates, trans_probs, initial_probs = generate_states_and_transitions(labeled_residue_array)
+    observed_transitions, trans_probs = compute_transition_probabilities(trans_probs, labeled_residue_array)
+    emission_probs = compute_emission_probabilities(column_state_array, total_sequence_array)
 
+    test_sequence = "VGQDH"
 
+    pHMM = HMM.HMM(alphabet=alphabet, hidden_states=hiddenstates, init_probs=initial_probs, trans_probs=trans_probs, emit_probs=emission_probs)
+    print(pHMM)
 
+    #print(total_sequence_array)
+    #print(column_state_array)
+    #print(labeled_residue_array[0, 0])
+    #print(hiddenstates)
+    #print(trans_probs)
+    #print(initial_probs)
+    #print(observed_transitions)
+    #prob, logprob = forward_sequence(test_sequence, pHMM)
+    #viterbi_path = viterbi_sequence(test_sequence, pHMM)
+    #print(logprob, viterbi_path)
 
-
-
-
-
-#print(total_sequence_array)
-#print(column_state_array)
-#print(labeled_residue_array[0, 0])
-#print(hiddenstates)
-#print(trans_probs)
-#print(observed_transitions)
-pHMM = HMM.HMM(alphabet="ARNDCEQGHILKMFPSTWYV", hidden_states=hiddenstates, init_probs=initial_probs, trans_probs=trans_probs, emit_probs=emission_probs)
-print(pHMM)
-#prob, logprob = forward_sequence(test_sequence, pHMM)
-#viterbi_path = viterbi_sequence(test_sequence, pHMM)
-#print(logprob, viterbi_path)
+if __name__ == "__main__":
+    main()
